b4a9ee4cb2c133fddffae75965836c4468e3b52c
KTryCode <32237739+KTryCode@users.noreply.github.com>
2017-10-24 22:41:39 +0200

    0.3
    Specyfied type of ArrayList

--- src/com/Krystiano/Main.java
+++ src/com/Krystiano/Main.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
-	    ArrayList items = new ArrayList();
+	    ArrayList<Integer> items = new ArrayList<>();
 	    items.add(1);
         items.add(2);
         items.add(3);
@@ -15,9 +15,9 @@ public class Main {
         printDoubled(items);
     }
 
-    private static void printDoubled(ArrayList n){
-        for(Object i: n){
-            System.out.println((Integer) i * 2);
+    private static void printDoubled(ArrayList<Integer> n){
+        for(int i: n){
+            System.out.println(i * 2);
         }
     }
 }
